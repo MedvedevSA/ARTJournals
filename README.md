d12e2e8b62d8ddc5b0f46b5be5314eae9a1921bd
Medvedev Stanislav <medvedevsa97@gmail.com>
2021-09-07 21:54:14 +0000

    upd gitignot

--- README.md
+++
@@ -1,2 +0,0 @@
-# ARTJournals
-ARTJournals
--- djangoreact/api/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResquet
-from django.shortcuts import render
-
-# Create your views here.
-
-def main (requset):
-    return HttpResquet('hello')
